ab59234f2d61b49637be7e5fc45a484ad2baf155
JSlupicki <not_important@not.exist.com>
2017-12-11 14:55:35 +0100

    Set up JUnit5, Spring Boot, Maven Wrapper

---
+++ src/main/java/com/slupicki/AkkaSprintTest.java
@@ -0,0 +1,12 @@
+package com.slupicki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkkaSprintTest {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AkkaSprintTest.class, args);
+    }
+}
